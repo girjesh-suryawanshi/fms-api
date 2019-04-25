13e2651950e6d24e26dac113a3e267ec17e8524b
Girjesh <girjeshlt@gmail.com>
2019-04-25 14:05:39 +0530

    @Component Scan Added

--- src/main/java/com/mpcz/fmsapi/FmsApiApplication.java
+++ src/main/java/com/mpcz/fmsapi/FmsApiApplication.java
@@ -2,8 +2,15 @@ package com.mpcz.fmsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.mpcz.fmsentity.bean","com.mpcz.fmsapi"})
+@EnableJpaRepositories(basePackages = {"com.mpcz.fmsdao.repository","com.mpcz.fmsapi"})
+@ComponentScan(basePackages = {"com.mpcz.fmsdao.dao","com.mpcz.fmsapi"})
 public class FmsApiApplication {
 
 	public static void main(String[] args) {
